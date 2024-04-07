b51e349f914bc856a40526e780a2ce3ad737a4c8
vanshjangir <vanshjangir0001@gmail.com>
2024-04-07 16:22:05 +0530

    f*cked up code submission, restarting

--- server/container/user_code.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-string input, output;
-cin >> input;
-cout << "-2 4 8 9 10";
-return 0;
-}
--- server/index.js
+++ server/index.js
@@ -6,8 +6,6 @@ const port = 3000
 const secret_key = "secret_key"
 const {auth} = require('./auth')
 const {admin} = require('./adminMiddleware')
-const fs = require('fs')
-const { exec } = require('child_process');
 
 const {MongoClient} = require('mongodb')
 const uri = "mongodb+srv://vansh:kWZ2MMhvZV2yJcst@hardcode.b6g1pwr.mongodb.net/?retryWrites=true&w=majority&appName=hardcode"
@@ -87,36 +85,7 @@ app.post('/submission', auth, async (req, res) => {
     const databaseProblem = await db.collection('problems').find({}).toArray();
     const givenProblem = databaseProblem.find(x => x.title === userSubmission.title);
     
-    fs.writeFileSync('./container/user_code.cpp', userSubmission.code, 'utf-8');
-    fs.writeFileSync('./container/input.txt', givenProblem.input, 'utf-8' );
-    fs.writeFileSync('./container/output.txt', givenProblem.output, 'utf-8');
-    fs.writeFileSync('./container/useroutput.txt', '','utf-8');
-    fs.writeFileSync('./container/error.txt', '','utf-8');
-    
-    const dockerCommand = `sudo docker run --rm \
-    -v ./user_code.cpp:/app/user_code.cpp \
-    -v ./input.txt:/app/input.txt \
-    -v ./output.txt:/app/output.txt \
-    -v ./useroutput.txt:/app/useroutput.txt \
-    -v ./error.txt:/app/error.txt code-runner`;
-
-    exec(dockerCommand, {cwd: './container/'}, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error: ${error}`);
-            res.status(500).send(`Error: ${error}`);
-            return;
-        }
-        
-        const fileoutput = fs.readFileSync('./container/useroutput.txt', 'utf-8');
-        console.log(fileoutput);
-        if(fileoutput == givenProblem.output){
-            res.status(200).json({result: "ACCEPTED"});     
-        }
-        else{
-            res.status(200).json({result: "WRONG ANSWER"});     
-        }
-    });
-
+    res.status(200).json({result: "WRONG ANSWER"});     
 })
 
 app.listen(port, () => {
